5838277489eedce573d505352ea9810834c7a526
Shekcon <quangsang9773@gmail.com>
2019-04-25 18:42:22 +0700

    Learning more knowlegde about JS and different with other programming languages

--- Basic-JS/main.js
+++ Basic-JS/main.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 // orderId = "200";
 // console.log(orderId);
 
@@ -14,6 +14,10 @@
 var integer = 100;
 var float = 1.24;
 console.log(typeof float);
+
+// string
+var str = 'dawdaw';
+console.log(typeof str);
 
 // boolean
 var isActive = false;
@@ -51,8 +55,7 @@ cancel('hello');
 
 // when function don't return will be undefined
 
-function cancel2() {
-}
+function cancel2() {}
 var order = cancel2();
 
 // for in
@@ -81,11 +84,114 @@ console.log(hello); // undefined
 var hello = 'hello';
 
 demo(); // running
-function demo(){
+function demo() {
     console.log('running');
 }
 
-test(); // TypeError
-var test = function(){
+// test(); // TypeError
+var test = function () {
     console.log('running');
-}                       
+}
+
+// oct number or hex number
+// var oct = 070; // 56
+// var hex = 0x02; // 2
+
+// use strict mode
+// var oct = 070; // 70
+
+// Number obj
+Number.MAX_VALUE
+Number.MIN_VALUE
+
+// Infinity
+var n = Number.MAX_VALUE * 2;
+console.log(n); // Infinity
+
+// Number
+console.log(undefined / 5);
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(typeof - Infinity);
+if (isNaN(undefined / 5))
+    console.log('isNaN');
+
+// String are immutable in JS
+console.log(typeof "dawdawd");
+
+var testStr = "  dawd ";
+testStr.trim();
+console.log(testStr);
+
+
+// Boolean
+console.log(typeof !!5);
+
+
+// undefined and null
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+console.log(undefined == null); // true
+console.log(undefined === null); // false
+
+// global scope
+var productID = "PRD-200";
+
+console.log(productID); // PRD-200
+console.log(window.productID); // PRD-200
+console.log(this === window); // true
+
+var description = 'software';
+
+function updatePD() {
+    description = 'new software';
+}
+
+updatePD();
+console.log('description:', description);
+
+// function scope
+var demoF = "hello";
+
+function updateDemo() {
+    var demoF = "world";
+}
+
+updateDemo();
+console.log('demoF:', demoF);
+
+// null = 0 when work with number
+// undefined work with number => NaN
+// null and undefined work with string => string
+// work with addition then JS see Str then convert all to string
+console.log("200" + 2); // 2002
+console.log("200" + null + undefined); // 200nullundefined
+console.log(2 + undefined); // NaN
+
+
+
+// subtraction
+console.log("200" - "1"); // 199
+console.log("PR300" - "PR100"); // NaN
+console.log("hello world", "200PR" - "100Hello");// NaN
+console.log(300 - null); // 300
+console.log(300 - undefined); // NaN
+
+
+var obj = {
+    valueOf: function () {
+        return 100;
+    }
+};
+var total = 300 - obj;
+console.log('total:', total);
+
+
+console.log(300 - NaN); // NaN
+
+
+console.log(1.1 * 2.1);// 2.3100000000000005
+console.log(false == 0); // true
+
+
